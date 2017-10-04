2ab7919e2fba051dc2d2300c00006cfc7aef0e1e
Alexey Miroshnychenko <muzz0neasdqwe@gmail.com>
2017-10-04 15:08:50 +0300

    restart

--- gulp/tasks/copy.fonts.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function() {
-  $.gulp.task('copy:fonts', function() {
-    return $.gulp.src('./source/fonts/**/*.*', { since: $.gulp.lastRun('copy:fonts') })
-      .pipe($.gulp.dest($.config.root + '/assets/fonts'));
-  });
-};
