9ae3a3b796d0bbf909c21df581570739f241293b
Alexey Miroshnychenko <muzz0neasdqwe@gmail.com>
2017-11-30 12:30:01 +0200

    add parallax

---
+++ js/parallax.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+
+var parallaxContainer = $('#parallax');
+var layers = $('#parallax li img');
+
+var moveLayers = function (e) {
+  var initialX = (window.innerWidth / 2) - e.pageX
+    initialY = (window.innerHeight / 2) - e.pageY;
+
+  [].slice.call(layers).forEach(function (layer, i) {
+    var
+      divider = i / 100, 
+      positionX = initialX * divider,
+      positionY = 0,
+      //bottomPosition = (window.innerHeight / 2) * divider,
+      layerStyle = layer.style,
+      transformString = 'translate3d(' + positionX + 'px ,' + positionY + 'px , 0)';
+
+    layerStyle.transform = transformString;
+    //layerStyle.bottom = '-' + bottomPosition + 'px';
+  });
+}
+
+window.addEventListener('mousemove', moveLayers);
+});
