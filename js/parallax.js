94b1e7ff45c177eb2dfcea80fb295477596f36af
Alexey Miroshnychenko <muzz0neasdqwe@gmail.com>
2017-12-04 13:43:28 +0200

    parallax ready

--- js/parallax.js
+++ js/parallax.js
@@ -9,17 +9,42 @@ var moveLayers = function (e) {
 
   [].slice.call(layers).forEach(function (layer, i) {
     var
-      divider = i / 100, 
+      divider = i / 100 + 0.01, 
       positionX = initialX * divider,
-      positionY = 0,
-      //bottomPosition = (window.innerHeight / 2) * divider,
+      positionY = initialY * divider,
       layerStyle = layer.style,
       transformString = 'translate3d(' + positionX + 'px ,' + positionY + 'px , 0)';
 
     layerStyle.transform = transformString;
-    //layerStyle.bottom = '-' + bottomPosition + 'px';
   });
 }
 
 window.addEventListener('mousemove', moveLayers);
 });
+
+
+
+// original //
+
+// var parallaxContainer = document.getElementById('parallax'),
+//   layers = parallaxContainer.children;
+
+// var moveLayers = function (e) {
+//   var initialX = (window.innerWidth / 2) - e.pageX
+//     initialY = (window.innerHeight / 2) - e.pageY;
+
+//   [].slice.call(layers).forEach(function (layer, i) {
+//     var
+//       divider = i / 100, 
+//       positionX = initialX * divider,
+//       positionY = initialY * divider,
+//       bottomPosition = (window.innerHeight / 2) * divider,
+//       layerStyle = layer.style,
+//       transformString = 'translate3d(' + positionX + 'px ,' + positionY + 'px , 0)';
+
+//     layerStyle.transform = transformString;
+//     layerStyle.bottom = '-' + bottomPosition + 'px';
+//   });
+// }
+
+// window.addEventListener('mousemove', moveLayers);
